53a4ab93ac6988ed3b0cc297c1a5c73ae2cdb56e
amunozo11 <amunozo@unicesar.edu.co>
2023-10-29 00:28:08 -0500

    avance que debe hacer nando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Sidebar from "./components/shared/Sidebar";
 
 function App() {
-
-
   return (
-    <>
-      <div className="bg-red-400">Hola mundo</div>
-    </>
+    <div className="bg-[#675378] w-full min-h-screen">
+      <Sidebar />
+    </div>
   )
 }
 
